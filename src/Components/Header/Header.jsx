db08105db181e5c77062a19b7ed06f65abde2638
pwrq <pwrdpwnz@gmail.com>
2022-07-21 06:16:19 +0300

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import s from './Header.module.scss'
+import logo from './../../Components/Header/Images/41da9c7a2c068fce396187fa91970bf9.webp'
+import {Link, NavLink} from "react-router-dom";
+
+
+
+
+const Header = () => {
+    return (<header className={s.header}>
+            <div className="container">
+                <nav className={s.nav}>
+                    <Link
+                        to="/">
+                        <img className={s.logo} src={logo} alt="Logo" title="back to home"/>
+                    </Link>
+                    <ul className={s.menu}>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/news'>NEWS</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/play'>BEGIN TO PLAY</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/restore'>PASSWORD CHANGE</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/forums'>FORUMS</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/statistic'>STATISTICS</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/knowledge'>KNOWLEDGE BASE</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/radio'>RADIO ASTERIOS</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/score'>SCORE</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/bonus'>GET BONUS</NavLink>
+                        </li>
+                        <li className={s.list}>
+                            <NavLink className={s.link} to='/about'>ABOUT THE SERVER</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>);
+};
+
+export default Header;
