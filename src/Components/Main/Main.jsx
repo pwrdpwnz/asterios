2f1618202301a465338ba3e9b39564fa2994334f
pwrq <pwrdpwnz@gmail.com>
2022-07-25 00:31:13 +0300

    add main bg

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import bgVideo from './video/bg.mp4'
 import SideBar from "../SideBar/SideBar";
-import s from './Main.module.scss'
+import s from './main.module.scss'
 
 
 const Main = () => {
     return (
-        <main className="main">
+        <main className={s.main}>
             <div className={s.wrapper}>
-                <SideBar/>
                 <div className={s.video}>
                     <video className={s.videoTag} autoPlay loop muted>
-                        <source src={bgVideo} type='video/mp4'/>
+                        <source src={bgVideo} type='video/mp4' />
                     </video>
                 </div>
+                <SideBar/>
             </div>
         </main>
     );
